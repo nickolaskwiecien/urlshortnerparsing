29ac9e5e5640d5246e97b2cb12f5fb2ad8e4d947
nkwiecien23 <kwiecinj@alfredstate.edu>
2018-07-10 15:39:21 -0400

    Update logparsingscript.ps1
    Please feel free to leave comments on how I can improve

--- logparsingscript.ps1
+++ logparsingscript.ps1
@@ -2,13 +2,16 @@ Get-Content 'C:\Users\nkwiecie\Downloads\Matched_hosts - Copy.csv' | ForEach-Obj
             
                 
                
+               #Using the select-string argument I can then use regex to look for specific data 
+               
                $url = Select-String  -InputObject $_  -pattern "(bit\.ly|goo\.gl|tinyurl\.com)\S*\s"
                $timestamp = select-string -InputObject $_ -pattern  "((\d+(:|-))+\d+\D)+"
                $websitetitle = Select-string -InputObject $_ -pattern "(Web Page Title:).*?(Web Page Visit|Browser)"
                $hostname = $_.split(",")
                $username = select-string -InputObject $_ -pattern "(Username: ).*?\s"
                
-             
+               #output to powershell to help with debuging 
+               
                Write-host ""
                Write-host " | Found URL ===" $url.matches.value 
                Write-host " | TIME STAMP === " $timestamp.matches.value 
@@ -19,7 +22,8 @@ Get-Content 'C:\Users\nkwiecie\Downloads\Matched_hosts - Copy.csv' | ForEach-Obj
           
             
 
-            
+               #Created custom objects from my extracted data
+               
                New-Object  -Type PSObject -Property @{
                'URL' = $url.Matches.value
                'timestamp' = $timestamp.matches.value
@@ -27,5 +31,5 @@ Get-Content 'C:\Users\nkwiecie\Downloads\Matched_hosts - Copy.csv' | ForEach-Obj
                'hostname' = $hostname.matches.value
                'username' = $username.matches.value 
               }
-
+                        #Finally exported to CSV 
                 } | Export-Csv C:\Users\nkwiecie\Downloads\output.csv
